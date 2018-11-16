e3447a39767b686f8c83a5e8224f2f8880649223
Mason Campbell <WaffleDefend29@gmail.com>
2018-11-16 08:15:07 -0700

    Fixed broken sql query

--- create_tables.sql
+++ create_tables.sql
@@ -1,32 +1,33 @@
-CREATE TABLE users (
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+CREATE TABLE IF NOT EXISTS users (
     id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
     user_name varchar(255),
     pass varchar(255),
     role varchar(255)
 );
 
-CREATE TABLE pages (
+CREATE TABLE IF NOT EXISTS pages (
     id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
     title varchar(255),
     content text
 );
 
-
-CREATE TABLE sub_pages (
+CREATE TABLE IF NOT EXISTS sub_pages (
     id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
     title varchar(255),
     content text,
-    -- INDEX main_page_id (id),
-    -- FOREIGN KEY (id) REFERENCES pages(id)
-    main_page_id FOREIGN KEY REFERENCES pages(id)    
+    main_page_id int,
+    FOREIGN KEY (main_page_id) REFERENCES pages(id)
 );
 
-CREATE INDEX main_page_id_index
-ON sub_pages (main_page_id);
-
-
-CREATE TABLE css (
+CREATE TABLE IF NOT EXISTS css (
     id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
     theme_name varchar(255),
     file_name varchar(255)
 );
+
+CREATE INDEX main_page_id_index
+ON sub_pages (main_page_id);
