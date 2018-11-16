f1fba49a42c7dd29735c3922148f86ad12073af8
Mason <lawrencemason00@gmail.com>
2018-11-16 08:48:36 -0700

    Update Frontend/script.js

---
+++ Frontend/script.js
@@ -0,0 +1,24 @@
+//switch styles
+//delete and add sub pages
+//auth users
+
+function AuthUser(){
+    //if user and pass == what is in db yes
+    //else no
+}
+
+function switchStyles(){
+    //admin only
+    //drop down
+    //get json data
+    //use it to populate the drop down
+
+}
+
+function deleteSubPage(page){
+    //admin only
+}
+
+function addSubPage(page){
+    //admin only
+}
