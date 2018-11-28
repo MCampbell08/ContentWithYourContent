146e2a5096d577aa9333ea0042592d87df05b562
Mason Campbell <waffledefend29@gmail.com>
2018-11-28 00:24:04 -0700

    Finished PHP files

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,6 +1,8 @@
 //switch styles
 //delete and add sub pages
 //auth users
+var data;
+var request = new XMLHttpRequest();
 
 function deleteSubPage(page){
     //admin only
@@ -11,8 +13,48 @@ function addSubPage(page){
 }
 
 function loadAboutData(title) {
-    request.open('GET', 'http://localhost:8000/contentwithyourcontent/frontend/about.php?title='+title);
+    request.open('GET', 'http://localhost/contentwithyourcontent/backend/about.php?title='+title);
     request.onload = loadComplete;
     request.send();
-  }
+}
   
+function loadContactData(title) {
+    request.open('GET', 'http://localhost/contentwithyourcontent/backend/contact.php?title='+title);
+    request.onload = loadComplete;
+    request.send();
+}
+  
+function loadEmailData(title) {
+    request.open('GET', 'http://localhost/contentwithyourcontent/backend/email.php?title='+title);
+    request.onload = loadComplete;
+    request.send();
+}
+  
+function loadHistoryData(title) {
+    request.open('GET', 'http://localhost/contentwithyourcontent/backend/history.php?title='+title);
+    request.onload = loadComplete;
+    request.send();
+}
+  
+function loadHomeData(title) {
+    request.open('GET', 'http://localhost/contentwithyourcontent/backend/index.php?title='+title);
+    request.onload = loadComplete;
+    request.send();
+}
+  
+function loadLocationsData(title) {
+    request.open('GET', 'http://localhost/contentwithyourcontent/backend/locations.php?title='+title);
+    request.onload = loadComplete;
+    request.send();
+}
+  
+function loadMissionData(title) {
+    request.open('GET', 'http://localhost/contentwithyourcontent/backend/mission.php?title='+title);
+    request.onload = loadComplete;
+    request.send();
+}
+  
+function loadComplete(evt) {
+    data = JSON.parse(request.responseText);
+    console.log(data);
+}
