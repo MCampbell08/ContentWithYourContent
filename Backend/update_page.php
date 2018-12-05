44333c531dd7b4e5c2d741e17af4a1e62733222f
Mason Campbell <WaffleDefend29@gmail.com>
2018-12-05 07:46:53 -0700

    fixed what i believe to be a working update functionality

--- Backend/update_page.php
+++ Backend/update_page.php
@@ -9,25 +9,23 @@
     
     
     if(isset($_GET['title'])) {
-		$title = filter_input(INPUT_GET, 'title', FILTER_SANITIZE_URL);
+		  $title = filter_input(INPUT_GET, 'title', FILTER_SANITIZE_URL);
     }
     if(isset($_GET['content'])) {
-		$content = urldecode(filter_input(INPUT_GET, 'content', FILTER_SANITIZE_URL));
+		  $content = urldecode(filter_input(INPUT_GET, 'content', FILTER_SANITIZE_URL));
     }
 
-    echo $content;
+    $query = "update pages set content = '".$content."' where title = '".$title.".html'";
 
-    // $query = "update pages set content = '".$content."' where title = '".$title.".html'";
+    $result = $mysqli->query($query);
 
-    // $result = $mysqli->query($query);
+    $query = "update sub_pages set content = '".$content."' where title = '".$title.".html'";
 
-    // $query = "update sub_pages set content = '".$content."' where title = '".$title.".html'";
+    $result = $mysqli->query($query);
 
-    // $result = $mysqli->query($query);
+    disconnect from database
 
-    //disconnect from database
-
-    // $mysqli->close();
+    $mysqli->close();
     
-    // echo $result;
+    echo $result;
 ?>
